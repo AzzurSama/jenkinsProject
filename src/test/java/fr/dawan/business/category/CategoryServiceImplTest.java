0f3a5a9a0db5e366bd039752593089770d1588ef
METRIAU Titouan <titouanmetriau.si@gmail.com>
2023-12-20 14:45:55 +0100

    test3

--- src/test/java/fr/dawan/business/category/CategoryServiceImplTest.java
+++ src/test/java/fr/dawan/business/category/CategoryServiceImplTest.java
@@ -1,15 +1,32 @@
 package fr.dawan.business.category;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CategoryServiceImplTest {
 
+    private CategoryServiceImpl service;
+
+    @Mock
+    private CategoryMapper mapper;
+
+    @Mock
+    private CategoryRepository repository;
+
+    @BeforeEach
+    void setup(){
+        service = new CategoryServiceImpl(repository, mapper);
+    }
 
     @Test
-    void passingTest() {
-        assertTrue(true);
+    void findAllTest() {
+
     }
 
 }
